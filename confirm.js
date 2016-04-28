bd7aeaecdbd1606cbda61d4fbd9fd625734da92a
php-require <onsilk.ru@ya.ru>
2016-04-28 17:20:50 +0300

    окно подтверждения при отмене  = false

--- confirm.js
+++ confirm.js
@@ -1,2 +1,3 @@
 confirm('This is an example of using JS to create some interaction on a website. Click OK to continue!');
-// окно подтверждения при отмене  = false 
+// окно подтверждения 
+//при отмене  = false 
