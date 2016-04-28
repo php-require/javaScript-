6c71e06004a9ad4c1c018301d87f1c99b58aea2a
php-require <onsilk.ru@ya.ru>
2016-04-28 19:22:32 +0300

    Добавление кнопки с вызывом окна

--- window.js
+++ window.js
@@ -1,2 +1,9 @@
-confirm('This is an example of using JS to create some interaction on a website. Click OK to continue!');
-prompt ("ti petuh"); // ввод данных из окна 
+confirm('This is an example of using JS to create some interaction on a website. Click OK to continue!'); // ok = true cancel = false 
+prompt ("what you name?"); // ввод данных из окна 
+
+//--вешаем окно на кнопку--//
+function show_alert() 
+{ 
+alert("Привет! Я сигнальное окно!"); 
+} 
+<input type="button" onclick="show_alert()" value="Показать сигнальное окно" /> 
