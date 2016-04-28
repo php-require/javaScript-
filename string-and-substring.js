820ccd069a06edb79a98b89ccdc1b8951d835dd7
php-require <onsilk.ru@ya.ru>
2016-04-28 20:30:50 +0300

    Работа со строками

---
+++ string-and-substring.js
@@ -0,0 +1,4 @@
+//---вырезка---// 
+console.log ( "January".substring(0, 3) ); //Jan
+console.log ( "Melbourne is great".substring(0, 12) ); // Melbourne is
+console.log ( "Hamburgers".substring(3, 10) ); //burgers
