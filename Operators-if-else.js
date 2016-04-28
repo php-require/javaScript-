0e15e02f2b555522127de6ccecd8d3de591e5771
php-require <onsilk.ru@ya.ru>
2016-04-28 20:29:27 +0300

    Простые варианты использования if else

---
+++ Operators-if-else.js
@@ -0,0 +1,53 @@
+if ( "siska-mashi".length >=7) {
+    console.log( "masha the best" );
+}
+
+// if and else
+
+if ( "hui".length >=8 ) 
+{
+    console.log("Let's go down the first road!");
+}
+else 
+{
+   console.log("pizdish"); 
+    
+    
+}
+
+// хитрость
+
+if ("condition" === false) 
+{
+ console.log ("The condition is true")   
+    
+}
+
+else {
+
+console.log ("The condition is false")
+    
+}
+
+// доп пример
+
+if (12 / 4 === "Ari".length) {
+    confirm("Will this run the first block?");
+} else {
+    confirm("Or the second block?");
+}
+
+
+//немного сложнее 
+
+  if 
+  
+  ("Jon".length * 2 / (2+1) === 2  ) 
+
+{
+    console.log("The answer makes sense!");
+} 
+else 
+{
+    console.log("Error Error Error") ;
+    }
